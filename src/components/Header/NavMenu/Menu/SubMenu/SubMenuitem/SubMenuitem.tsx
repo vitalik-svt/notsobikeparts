aa09768e9c90ae3b7f9036141abe2afa0fa1e047
demidemid <iamdemid@gmail.com>
2025-09-26 15:00:53 +0300

    Enhance SubMenuitem component with touch device support and improved styling

--- src/components/Header/NavMenu/Menu/SubMenu/SubMenuitem/SubMenuitem.tsx
+++ src/components/Header/NavMenu/Menu/SubMenu/SubMenuitem/SubMenuitem.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { MenuItem } from "@/types/menu";
 import { useState } from "react";
+import { useIsTouchDevice } from "../../../useIsTouchDevice";
 
 interface Props {
     subitem: MenuItem;
@@ -9,27 +10,36 @@ interface Props {
 }
 
 export default function SubMenuitem({ subitem, hasLink }: Props) {
-    const [collapsed, setCollapsed] = useState(true);
+    const isTouch = useIsTouchDevice();
+    const [collapsed, setCollapsed] = useState(isTouch);
+
+    const onHandleClick = () => {
+        if (isTouch) {
+            setCollapsed(prev => !prev)
+        }
+    }
+
+    console.log('isTouch  | collapsed', isTouch, collapsed)
 
     return (
-        <li key={subitem.label} className={`text-lg md:w-[25%] ${subitem.submenu && "md:pb-65"}`}>
+        <li key={subitem.label} className={`group/sub text-lg md:w-[25%] ${subitem.submenu && "md:pb-65"}`}>
             {hasLink ? (
-                <Link href={subitem.href || ``} className="block px-4 py-2 hover:bg-gray-100 ">
+                <Link href={subitem.href || ``} className="block px-4 py-2 md:hover:text-blue-500">
                     {subitem.label}
                 </Link>
             ) : (
                 <>
                     <button
                         className="flex justify-between items-stretch w-full border-b md:border-0 relative before:absolute before:top-0 before:left-0 before:w-5 before:h-full before:bg-black before:content-[''] md:before:hidden md:justify-start"
-                        onClick={() => setCollapsed(prev => !prev)}
+                        onClick={onHandleClick}
                     >
-                        <span className="block w-full text-left ps-10 py-2 hover:text-blue-500 md:!px-2 md:w-auto">
+                        <span className={`block w-full text-left ps-10 py-2 md:!px-2 md:w-auto ${subitem.submenu ? "" : "hover:text-blue-500"}`}>
                             {subitem.label}
                         </span>
                         {subitem.submenu && (
                             <span className="flex items-center border-s shrink-0 px-4 md:!border-0 md:px-0">
                                 <Image
-                                    className={`${collapsed ? "" : "rotate-180"} md:w-[20px] md:h-[20px]`}
+                                    className={`transition-transform ${isTouch ? (collapsed ? "" : "rotate-180") : ""} md:w-[20px] md:h-[20px] md:group-hover/sub:rotate-180`}
                                     src="/icons/arrow-down.webp"
                                     alt="Icon"
                                     width={28}
@@ -39,12 +49,15 @@ export default function SubMenuitem({ subitem, hasLink }: Props) {
                         )}
                     </button>
 
-                    {subitem.submenu && subitem.submenu.length > 0 && !collapsed && (
-                        <ul className="flex flex-col divide-y md:border-0 md:divide-y-0 md:mt-2 md:flex-row items-start text-center md:absolute md:w-[925px] md:left-5 md:gap-2">
-                            {subitem.submenu.map(grandchild => (
+                    {(
+                        <ul className={
+                            `flex-col divide-y md:border-0 md:divide-y-0 md:mt-2 md:flex-row items-start text-center md:absolute md:w-[925px] md:left-5 md:gap-2
+                            ${isTouch ? (collapsed ? "hidden" : "flex") : "hidden md:group-hover/sub:flex"}`
+                        }>
+                            {subitem.submenu?.map(grandchild => (
                                 <li key={grandchild.label} className="last:border-b w-full text-left md:w-[20%] md:border-0 md:py-0 md:last:border-b-0">
                                     {grandchild.href && (
-                                        <Link href={grandchild.href} className="block px-15 py-2 hover:bg-gray-100 relative before:absolute before:top-0 before:left-0 before:w-10 before:h-full before:bg-black before:content-[''] md:before:hidden md:text-left md:px-2 md:w-full">
+                                        <Link href={grandchild.href} className="block px-15 py-2 md:hover:text-blue-500 relative before:absolute before:top-0 before:left-0 before:w-10 before:h-full before:bg-black before:content-[''] md:before:hidden md:text-left md:px-2 md:w-full">
                                             {grandchild.label}
                                         </Link>
                                     )}

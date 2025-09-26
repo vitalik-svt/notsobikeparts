d991e9d82be2db601a5cae054014b2a82c57b412
demidemid <iamdemid@gmail.com>
2025-09-26 15:26:42 +0300

    Refactor layout components to use dynamic desktop width from constants; update Footer, Header, and Main components for consistent styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,6 @@
+import { DESKTOP_WIDTH } from "@/constants/contacts";
 import { PropsWithChildren } from "react";
 
 export default function Main({ children }: PropsWithChildren) {
-    return <main className="flex flex-1 max-w-[1000px] mx-auto w-full">{children}</main>;
+    return <main className={`flex flex-1 max-w-[${DESKTOP_WIDTH}px] mx-auto w-full`}>{children}</main>;
 }
--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,3 +1,5 @@
+export const DESKTOP_WIDTH = 1200;
+
 export const CONTACTS = {
   EMAIL: 'info@notsobikeparts.com',
   INSTAGRAM: 'https://instagram.com/notsobikeparts',

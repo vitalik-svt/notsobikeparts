ef61db9fc8c55b75c14c701f2afd40d6c125ed06
demidemid <iamdemid@gmail.com>
2025-10-27 13:51:59 +0300

    feat: Refactor CartPage to use OrderSummary and OrderCheckout components, removing CartTable and related logic

--- src/app/[locale]/cart/page.tsx
+++ src/app/[locale]/cart/page.tsx
@@ -1,45 +1,22 @@
 'use client';
 
-import Button from "@/components/Button/Button";
-import CartTable from "@/components/CartTable/CartTable";
+import OrderCheckout from "@/components/OrderCheckout/OrderCheckout";
+import OrderSummary from "@/components/OrderSummary/OrderSummary";
 import ProductPage from "@/components/ProductPage/ProductPage";
-import { cartStore } from "@/stores/cartStore";
-import { formatPrice } from "@/utils/formatPrice";
-import { useMemo } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function CartPage() {
+    const [orderStep, setOrderStep] = useState<'summary' | 'checkout' | 'done'>('summary');
     const { t } = useTranslation();
-    const { items, totalCount } = cartStore();
-
-    const totalPrice = useMemo(() => items.reduce((total, item) => total + item.price.amount * item.quantity, 0), [items]);
-    const priceForFormatting = items[0]?.price
-        ? { ...items[0].price, amount: totalPrice }
-        : { amount: totalPrice, currency: "USD", locale: "en-US" };
 
     return (
         <ProductPage>
             <section className="flex flex-col gap-3 grow pt-5">
                 <h1 className="text-3xl font-bold">{t("cart.title")}</h1>
 
-                {totalCount !== 0 ? (
-                    <>
-                        <CartTable items={items} />
-                        <div className="flex flex-col gap-2 text-right items-end w-4/12 ms-auto pt-7">
-                            <p className="uppercase font-bold ">
-                                {t("cart.total_price", { priceWithCurrency: formatPrice(priceForFormatting) })}
-                            </p>
-                            <Button
-                                onClick={() => { }} fluid
-                            >
-                                {t('cart.checkout')}
-                            </Button>
-                        </div>
-                    </>
-                ) : (
-                    <p className="flex min-h-10 items-center justify-center grow">{t("cart.empty")}</p>
-                )}
-
+                {orderStep === 'summary' && <OrderSummary onClick={() => setOrderStep('checkout')} />}
+                {orderStep === 'checkout' && <OrderCheckout onClick={() => setOrderStep('done')} />}
 
             </section>
 
--- src/components/OrderSummary/CartTable/ProductOptionParams/OptionRow/OptionRow.tsx
+++ src/components/OrderSummary/CartTable/ProductOptionParams/OptionRow/OptionRow.tsx
@@ -1,6 +1,6 @@
 export default function OptionRow({ label, value }: { label?: string; value: string }) {
     return (
-        <p className="flex gap-1">
+        <p className="flex gap-1 justify-between md:justify-start">
             {label && <span className="font-bold">{label}:</span>}
             <span>{value}</span>
         </p>

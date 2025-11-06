f20554e7f74d1f8669ea4869a4d54d4e0bfd779b
demidemid <iamdemid@gmail.com>
2025-11-06 17:55:23 +0300

    feat: Add Itchy and Scratchy product images and update localization keys

--- src/app/[locale]/others/itchy-and-scratchy/page.tsx
+++ src/app/[locale]/others/itchy-and-scratchy/page.tsx
@@ -4,7 +4,6 @@ import Gallery from "@/components/Gallery/Gallery";
 import ProductMain from "@/components/ProductPage/ProductMain/ProductMain";
 import ProductMainInfo from "@/components/ProductPage/ProductMain/ProductMainInfo/ProductMainInfo";
 import ProductPage from "@/components/ProductPage/ProductPage";
-import SectionInfoBlock from "@/components/SectionInfoBlock/SectionInfoBlock";
 import { useItchyAndScratchyData } from "@/hooks/useItchyAndScratchyData";
 
 export default function ItchyAndScratchyPage() {
@@ -13,20 +12,15 @@ export default function ItchyAndScratchyPage() {
     return (
         <ProductPage>
             <ProductMain>
-                <Gallery images={data.serial.images} />
+                <Gallery images={data.images} />
                 <ProductMainInfo
-                    title={data.serial.title}
-                    price={data.serial.price}
-                    description={data.serial.description[0]}
+                    title={data.name}
+                    // TODO: fix price
+                    // price={data.price}
+                    description={data.description[0]}
                 >
-                    <p>{data.serial.description[1]}</p>
-                    <p>{data.serial.description[2]}</p>
-                    <SectionInfoBlock title={data.serial.equipment.title}>
-                        {data.serial.equipment.items.map((item, index) => (
-                            <p key={index}>{item}</p>
-                        ))}
-                    </SectionInfoBlock>
-                    
+                    <p>{data.description[1]}</p>
+                    <p>{data.description[2]}</p>
                 </ProductMainInfo>
             </ProductMain>
         </ProductPage>
--- src/hooks/useItchyAndScratchyData.ts
+++ src/hooks/useItchyAndScratchyData.ts
@@ -6,4 +6,20 @@ import { useTranslation } from "react-i18next";
 export function useItchyAndScratchyData() {
     const locale = (useLocale() || i18n.defaultLocale) as Locales;
     const { t } = useTranslation('itchyAndScratchy');
+
+    console.log('locale', locale)
+
+    const data = {
+        name: t(`itchy_scratchy.name`),
+        images: [
+            "/images/itchy-and-scratchy/gallery/product-pic-1.avif",
+        ],
+        description: [
+            t(`itchy_scratchy.description.1`),
+            t(`itchy_scratchy.description.2`),
+            t(`itchy_scratchy.description.3`),
+        ],
+    }
+
+    return { data };
 }

837c07be676454ad19b90d700e7c3ede244bec78
demidemid <iamdemid@gmail.com>
2025-11-06 16:12:29 +0300

    fix: Refactor locale handling in LocalizedNotFound component for improved clarity and consistency

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,20 +5,19 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ruCommon from "../../public/locales/ru/common.json";
 import enCommon from "../../public/locales/en/common.json";
+import { i18n } from "@/i18n/settings";
+import { Locales } from "@/types/locales";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const COMMON: Record<string, any> = {
+const COMMON: Record<Locales, any> = {
     ru: ruCommon,
     en: enCommon,
 };
 
 export default function LocalizedNotFound() {
     const pathname = usePathname() ?? '/';
-    const locale = pathname.split('/')[1] || 'en';
-
-    console.log('COMMON', COMMON)
-
-    const ns = COMMON[locale] ?? COMMON['en'];
+    const locale = (pathname.split('/')[1] || i18n.defaultLocale) as Locales;
+    const ns = COMMON[locale] ?? COMMON[i18n.defaultLocale];
 
     const msg = {
         title: ns['notFound.title'] ?? 'Page not found',
@@ -35,7 +34,7 @@ export default function LocalizedNotFound() {
                     <span aria-hidden="true" className=" text-black font-extrabold text-[10rem] pointer-events-none select-none">4</span>
                 </div>
                 <h1 className="text-3xl font-bold mb-14 leading-none">{msg.title}</h1>
-                <Link href={`/${locale}/`} className="inline-block lowercase rounded border px-4 py-2">
+                <Link href={`/${locale}/`} className="inline-block lowercase rounded bg-black text-white px-4 py-2">
                     {msg.home}
                 </Link>
             </div>

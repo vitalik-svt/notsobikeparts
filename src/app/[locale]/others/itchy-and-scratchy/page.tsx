5b8c017381ea02e2b51a76e3902ab41fb8633056
demidemid <iamdemid@gmail.com>
2025-11-10 22:44:13 +0300

    feat: Enhance Itchy and Scratchy page with cart functionality and improved localization

--- src/app/[locale]/others/itchy-and-scratchy/page.tsx
+++ src/app/[locale]/others/itchy-and-scratchy/page.tsx
@@ -6,21 +6,45 @@ import ProductGridCard from "@/components/ProductGrid/ProductGridCard/ProductGri
 import ProductMain from "@/components/ProductPage/ProductMain/ProductMain";
 import ProductMainInfo from "@/components/ProductPage/ProductMain/ProductMainInfo/ProductMainInfo";
 import ProductPage from "@/components/ProductPage/ProductPage";
+import { ProductPriceSettings } from "@/constants/productPrices";
 import { CoatingType, useItchyAndScratchyData } from "@/hooks/useItchyAndScratchyData";
 import useProductOptionDictionary from "@/hooks/useProductOptionDictionary";
+import { CageColor, CagePlusColor, cartStore } from "@/stores/cartStore";
 import { formatPrice } from "@/utils/formatPrice";
+import { usePathname } from "next/navigation";
 import { useTranslation } from "react-i18next";
 
 export default function ItchyAndScratchyPage() {
-    const { t } = useTranslation();
+    const productLink = usePathname();
+    const { t: tCommon } = useTranslation();
     const { t: tCages } = useTranslation(`cages`);
     const { data } = useItchyAndScratchyData();
     const optionDictionary = useProductOptionDictionary();
+    const { addItem } = cartStore();
+
 
     const paintedTypeLabel: Record<CoatingType, string> = {
         anodized: tCages('plus.color.anodized'),
         powder: tCages('plus.color.painted'),
     };
+
+    const addToCart = ({ imageUrl, title, productParams, price }: {
+        imageUrl: string; title: string, productParams: {
+            cageColor: CageColor | CagePlusColor;
+            paintedType: CoatingType;
+        }, price: ProductPriceSettings
+    }) => {
+        addItem({
+            id: `itchy-and-scratchy-${productParams.cageColor}-${productParams.paintedType}`,
+            imageUrl,
+            title,
+            productParams,
+            price,
+            quantity: 1,
+            productLink
+        })
+    }
+
 
     return (
         <ProductPage>
@@ -40,17 +64,22 @@ export default function ItchyAndScratchyPage() {
                         <ProductGridCard
                             description={item.description.join(' ')}
                             url={item.images[0]}
-                            selectProduct={() => { }}
+                            selectProduct={() => addToCart({
+                                imageUrl: item.images[0],
+                                title: item.name,
+                                productParams: item.productParams,
+                                price: item.price
+                            })}
                             title={item.name}
                             addInfo={(
                                 <>
                                     <p className="font-bold">{formatPrice(item.price)}</p>
                                     <div className="lowercase text-sm">
                                         <OptionRow
-                                            label={t("cart.color_label")}
+                                            label={tCommon("cart.color_label")}
                                             value={optionDictionary[item.productParams.cageColor]} />
                                         <OptionRow
-                                            label={t("coatingTypeLabel")}
+                                            label={tCommon("coatingTypeLabel")}
                                             value={paintedTypeLabel[item.productParams.paintedType]} />
                                     </div>
                                 </>

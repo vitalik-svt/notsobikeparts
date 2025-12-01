67ef8fc37054352e63d065c0d9656357997416fe
demidemid <iamdemid@gmail.com>
2025-12-01 18:47:06 +0300

    refactor: Simplify HomePage component by removing async i18n initialization

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
-import { initI18n } from "@/i18n";
-import Image from "next/image";
+'use client';
 
-export default async function HomePage({ params }: { params: Promise<{ locale: string }> }) {
-  const { t } = await initI18n((await params).locale);
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
+
+export default function HomePage() {
+	const { t } = useTranslation();
 
 	return (
 		<div className="flex flex-col items-center justify-center w-full p-8 pb-20 gap-16 font-sans sm:p-20 md:flex-row md:gap-32">
